d5e9d104a0b7fedcfdbe5790a9b8df1f2df6d6ec
alexcurtis <alejandro.curtis@my.jcu.edu.au>
2025-09-23 15:17:07 +1000

    Refactor password program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,22 @@
 """Password checking"""
 
-minimum_length = 1
-password = input("Enter password: ")
-while len(password) < minimum_length:
-    print("Invalid")
+def main():
+    minimum_length = 8
+    password = get_password(minimum_length)
+    print_password(password)
+
+
+def print_password(password):
+    print("*" * len(password))
+
+
+def get_password(minimum_length):
     password = input("Enter password: ")
-print("*" * len(password))
+    while len(password) < minimum_length:
+        print("Invalid")
+        password = input("Enter password: ")
+    return password
+
+
+main()
 
