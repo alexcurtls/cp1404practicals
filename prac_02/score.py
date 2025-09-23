a8cb461c18e8ed7aeca87a893f51a2daec638edc
alexcurtis <alejandro.curtis@my.jcu.edu.au>
2025-09-23 15:55:46 +1000

    Convert score program into functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,21 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Main function to input score"""
+    score = float(input("Enter score: "))
+    print(determine_grade(score))
+
+
+def determine_grade(score):
+    """Determine grade based on score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
