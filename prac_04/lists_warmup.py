961bb7e33c10e922858bb1360c067ce581d84ee5
alexcurtis <alejandro.curtis@my.jcu.edu.au>
2025-10-14 09:28:03 +1000

    add questions and answers

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 3, 2, 1, [3, 1, 4, 1, 5, 9], [1], true, false, false,
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# 1.
+numbers[0] = "ten"
+# 2.
+numbers.append(1)
+# 3.
+if 9 not in numbers:
+    print("Nuh uh")
+
+
